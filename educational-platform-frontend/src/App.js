5fb68817722d2a7f1fa64708990278fc0ca03591
Skyler-18 <hardik.goriwan@gmail.com>
2025-02-09 20:42:13 +0530

    DEMO CHANGE

--- educational-platform-frontend/src/App.js
+++ educational-platform-frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          THIS IS SE PROJECT.
         </a>
       </header>
     </div>
