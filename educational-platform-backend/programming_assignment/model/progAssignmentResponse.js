d5d8a819978dd8f87155d39eb312ba2a1a4c7d84
Satyam-srivastava <21f1000629@ds.study.iitm.ac.in>
2025-03-27 17:31:34 +0530

    kuch bhi kar lo hamse nahi ho payega, isse ganda ab kya hi hoga

--- educational-platform-backend/programming_assignment/model/progAssignmentResponse.js
+++ educational-platform-backend/programming_assignment/model/progAssignmentResponse.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const progAssignmentResponseSchema = new mongoose.Schema({
   user_id: { type: String, required: true },
-  assignment_id: { type: mongoose.Schema.Types.ObjectId, ref: "ProgAssignment", required: true },
+  assignment_id: { type: String, required: true },
   course_id: { type: String, required: true },  // ✅ Change this from ObjectId to String
   response: { type: String, required: true },
-  actual_output: { type: String, required: true },
+  actual_solution: { type: String, required: true },
   submitted_on: { type: Date, default: Date.now },
 });
 
